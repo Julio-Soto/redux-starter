9d0e88e8507530028d0015e41f65dfee648c3ce0
Julio Soto <juliosotodev@gmail.com>
2022-09-14 23:27:13 -0600

    added reducer

---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+
+
+function reducer(state,action) {
+    switch(action.type) {
+        case 'bugAdded':
+            return [
+                ...state,
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ]
+        case 'bugRemoved':
+            return state.filter(bug => BigInt.id === action.payload.id)
+        default:
+            return state
+    }
+}
