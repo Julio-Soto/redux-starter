8036b0df98353ab9e9aae161a6085147f12971ae
Julio Soto <juliosotodev@gmail.com>
2022-09-16 23:14:04 -0600

    added action types

--- src/actions.js
+++ src/actions.js
@@ -1,20 +1,15 @@
+import * as actions from './actionTypes'
 
-
-export const addBug = (description) => {
-    return {
-        type: 'bugAdded',
+export const addBug = (description) => ({
+        type: actions.BUG_ADDED,
         payload: {
             description: description
         }
-    }
-}
+    })
 
-export const removeBug = (id) => {
-    console.log('remove id: ' + id)
-    return {
-        type: 'bugRemoved',
+export const removeBug = (id) => ({
+        type: actions.BUG_REMOVED,
         payload: {
             id: id
         }
-    }
-}
+    })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import store from './store'
 import { addBug,removeBug } from './actions'
 
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+})
 
 store.dispatch(addBug('terrible bug'))
 store.dispatch(addBug('Awesome bug'))
@@ -8,4 +11,4 @@ store.dispatch(addBug('boring bug'))
 
 store.dispatch(removeBug(2))
 
-console.log(store.getState())
+unsubscribe()
